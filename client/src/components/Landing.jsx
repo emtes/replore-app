f75eaded2ec716cb6a8102baf2867037ca4ece15
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-05-10 00:49:03 -0400

    add title and desc to landing page

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <main>
+      <h1>Replore</h1>
+      <p>Explore Federal Repositories</p>
+    </main>
+  );
+}
+
+export default Landing;
