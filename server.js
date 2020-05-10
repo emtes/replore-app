e70415bc1c08a59769a3340878cbbf0d869ff472
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-05-10 16:11:42 -0400

    move functions to not be used before declaration

--- server.js
+++ server.js
@@ -5,36 +5,31 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/fed-repos', async (req, res) => {
-  try {
-    const repos = await getFedRepos();
-    res.status(200).json({ results: repos });
-  } catch (e) {
-    res.status(500).json({ error: '500 Failed to load resource.', details: e });
-  }
-});
-
-app.get('/fed-languages', async (req, res) => {
-  try {
-    const languages = await getFedLangs();
-    res.status(200).json({ results: languages });
-  } catch (e) {
-    res.status(500).json({ error: '500 Failed to load resource.', details: e });
-  }
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+/**
+ * Makes an authorized requeset to the specified Code.gov API endpoint
+ * @param {string} endPoint - route appended to base URL
+ * @param {number} size - amount of repos returned for /repos
+ */
+const codeGovReq = async (endPoint, size) => {
+  const BASE_URL = 'https://api.code.gov';
+  const sizeQuery = size ? `?size=${size}` : '';
+  const reqInit = {
+    headers: { 'x-api-key': process.env.GOV_API_KEY },
+  };
+  const res = await fetch(BASE_URL + endPoint + sizeQuery, reqInit);
+  return res.json();
+};
 
 /**
  * Consumes response from /repos and cleans it for this usecase
  * @returns {Array} Array of objects representing repositories
  * @param {number} size - Amount of repositories to fetch. 300 default.
  */
-async function getFedRepos(size = 1000) {
-  // arbitrary default, not too many/little. API default = 10, max = 1000
+const getFedRepos = async (size = 1000) => {
+  // max = 1000, already takes long enough to load client-side
   const { repos } = await codeGovReq('/repos', size);
 
-  return repos.map(repo => {
+  return repos.map((repo) => {
     const {
       date,
       homepageURL,
@@ -47,9 +42,9 @@ async function getFedRepos(size = 1000) {
       tags,
       vcs,
       agency,
-      score
+      score,
     } = repo;
-    // not incredibly uniform data
+    // not incredibly uniform data, for easier checks client-side
     return {
       date: date || null,
       homepageURL: homepageURL || null,
@@ -63,36 +58,42 @@ async function getFedRepos(size = 1000) {
       vcs: vcs || null,
       agency: agency
         ? {
-            name: agency.name,
-            acronym: agency.acronym,
-            website: agency.website
-          }
+          name: agency.name,
+          acronym: agency.acronym,
+          website: agency.website,
+        }
         : null,
-      score: score || null
+      score: score || null,
     };
   });
-}
+};
+
+app.get('/fed-repos', async (req, res) => {
+  try {
+    const repos = await getFedRepos();
+    res.status(200).json({ results: repos });
+  } catch (e) {
+    res.status(500).json({ error: '500 Failed to load resource.', details: e });
+  }
+});
 
 /**
  * Consumes response from /languages and formats it for usecase
  * @returns {Array} Array of strings representing languages
  */
-async function getFedLangs() {
+const getFedLangs = async () => {
   const { languages } = await codeGovReq('/languages');
-  return languages.sort((a, b) => b.numRepos - a.numRepos).map(l => l.name);
-}
+  // repo counds useful since they're from total and I just show a sample
+  return languages.sort((a, b) => b.numRepos - a.numRepos).map((l) => l.name);
+};
 
-/**
- * Makes an authorized requeset to the specified Code.gov API endpoint
- * @param {string} endPoint - route appended to base URL
- * @param {number} size - amount of repos returned for /repos
- */
-async function codeGovReq(endPoint, size) {
-  const BASE_URL = 'https://api.code.gov';
-  size = size ? `?size=${size}` : '';
-  const reqInit = {
-    headers: { 'x-api-key': process.env.GOV_API_KEY }
-  };
-  const res = await fetch(BASE_URL + endPoint + size, reqInit);
-  return res.json();
-}
+app.get('/fed-languages', async (req, res) => {
+  try {
+    const languages = await getFedLangs();
+    res.status(200).json({ results: languages });
+  } catch (e) {
+    res.status(500).json({ error: '500 Failed to load resource.', details: e });
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
