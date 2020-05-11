7109945d8bb4861a17e5b3789c917804b3798c9c
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-05-11 00:23:36 -0400

    add build

---
+++ client/build/precache-manifest.15196e5558522f3f7ed67dfff67d4998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe1b3eba9fae37bae0796f59d7d6b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "613f5859f8fe52489598",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "613f5859f8fe52489598",
+    "url": "/static/js/2.6dc7faeb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6dc7faeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d62ec6264cdcba71a5",
+    "url": "/static/js/main.79511a6c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
